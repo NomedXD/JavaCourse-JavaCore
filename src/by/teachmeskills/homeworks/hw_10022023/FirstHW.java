9799488bb9893389a7f6d4a19f03487c3fa56351
Matoshka Ivan <coovshow1@gmail.com>
2023-02-02 14:03:18 +0300

    Difference of 2 int

--- src/by/teachmeskills/homeworks/hw_10022023/FirstHW.java
+++ src/by/teachmeskills/homeworks/hw_10022023/FirstHW.java
@@ -4,7 +4,7 @@ public class FirstHW {
     public static void main(String[] args) {
         int a=7;
         int b=10;
-        int res = a+b;
+        int res = a-b;
         System.out.println(res);
     }
 
