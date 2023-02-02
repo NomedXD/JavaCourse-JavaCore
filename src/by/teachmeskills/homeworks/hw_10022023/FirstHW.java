efc0c5b2fd4cd16aa55c685f6314744b1cf1a1fc
Matoshka Ivan <coovshow1@gmail.com>
2023-02-02 13:58:49 +0300

    Sum of 2 int

---
+++ src/by/teachmeskills/homeworks/hw_10022023/FirstHW.java
@@ -0,0 +1,11 @@
+package by.teachmeskills.homeworks.hw_10022023;
+
+public class FirstHW {
+    public static void main(String[] args) {
+        int a=7;
+        int b=10;
+        int res = a+b;
+        System.out.println(res);
+    }
+
+}
