d13cf29f30230417ba62f8df807f70e3ee46a638
NomedXD <coovshow1@gmail.com>
2023-05-04 18:47:29 +0300

    hw_05052023

---
+++ src/by/teachmeskills/homeworks/hw_05052023/task1/Product.java
@@ -0,0 +1,4 @@
+package by.teachmeskills.homeworks.hw_05052023.task1;
+
+public record Product(String name, int cost) {
+}
---
+++ src/by/teachmeskills/homeworks/hw_05052023/task1/Run.java
@@ -0,0 +1,48 @@
+package by.teachmeskills.homeworks.hw_05052023.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Run {
+    private static boolean areAllResultsReady(List<Future<Integer>> results) {
+        for (Future<Integer> element : results) {
+            if (!element.isDone()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int calculateTotalGain(List<Future<Integer>> results) {
+        int result = 0;
+        try {
+            for (Future<Integer> element : results) {
+                result += element.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(15);
+        GameShop shop = new GameShop("GabeStore.ru");
+        threadPool.execute(shop);
+        List<Future<Integer>> purchasesCosts = new ArrayList<>();
+        for (int i = 0; i < 10; ++i) {
+            Future<Integer> purchaseCost = threadPool.submit(new Customer("Customer " + i, shop));
+            purchasesCosts.add(purchaseCost);
+        }
+        while (!areAllResultsReady(purchasesCosts)) {
+            System.out.println("Waiting for result...");
+            Thread.sleep(5000);
+        }
+        System.out.println("Total purchased goods cost: " + calculateTotalGain(purchasesCosts));
+        threadPool.shutdown();
+    }
+}
