92943d43b7652b5a86732850981bed3a8027c3e9
NomedXD <coovshow1@gmail.com>
2023-05-15 23:02:06 +0300

    hw_19052023

---
+++ javaEEtask1/src/main/java/by/teachmeskills/task1/Calculator.java
@@ -0,0 +1,19 @@
+package by.teachmeskills.task1;
+
+public class Calculator {
+    private Calculator(){
+
+    }
+    public static double summation(double num1, double num2){
+        return num1 + num2;
+    }
+    public static double subtraction(double num1, double num2){
+        return num1 - num2;
+    }
+    public static double multiplication(double num1, double num2){
+        return num1 * num2;
+    }
+    public static double division(double num1, double num2){
+        return num1 / num2;
+    }
+}
